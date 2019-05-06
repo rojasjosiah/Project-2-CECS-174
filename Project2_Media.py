2af5771e7e599ce8936729c4c5baeb867afc23ed
rojasjosiah <rojasjosiah@gmail.com>
2019-05-06 01:02:04 -0700

    General edits and bugfixes

--- Project2_Media.py
+++ Project2_Media.py
@@ -7,6 +7,7 @@ Josiah Rojas: (Project Manager)
     - Contributed to comments of Media, Book, and Video classes (4/25)
     - added framework class Member and its basic methods w/o capabilities (4/28)
     - Edited displayStats function (5/5)
+    - Contributing to editing program for redundancy (5/5)
 April Picato:
 
 Philip Ramirez:
@@ -17,7 +18,7 @@ Ali Mourad:
 Miguel Zavala:
     -Contributed to Program debugging (5/1)
     -Contributed to methods of the Member class (5/1)
-    -Created method to display stats and contributed to it's print formatting (5/2)
+    -Created method to display stats and contributed to its print formatting (5/2)
 
 '''
 
@@ -33,6 +34,7 @@ class Media:
         self.title = title
         self.author = author
         self.publisher = publisher
+        Media.addMedia(self)
 
         # Method is used to add books/videos to be checked out
         # PS objects should be pass through here
@@ -64,27 +66,25 @@ class Media:
 # class Book child of class Media, creates Book type for library
 class Book(Media):
     book_count = 0
-    book_library = {}
 
     def __init__(self, title, author, publisher, num_pages):
         super().__init__(title, author, publisher)
         self.num_pages = num_pages
         Book.book_count += 1
-        book_descr = "Author: %s, Publisher: %s, Run time: %" % (author, publisher, num_pages)
-        Book.book_library[title] = book_descr
+        # changed to descr so wouldn't have to know type of media to use
+        self.descr = "Author: %s, Publisher: %s, Number of Pages: %s" % (author, publisher, num_pages)
+
 
 
 # class Video child of class Media, creates Video type for library
 class Video(Media):
     video_count = 0
-    video_library = {}
 
     def __init__(self, title, author, publisher, run_time):
         super().__init__(title, author, publisher)
         self.run_time = run_time
         Video.video_count += 1
-        video_descr = "Author: %s, Publisher: %s, Run time: %" % (author, publisher, run_time)
-        Video.video_library[title] = video_descr
+        self.descr = "Author: %s, Publisher: %s, Run time: %s" % (author, publisher, run_time)
 
 
 # Member class for library members, feel free to code in those check in/out methods
@@ -103,7 +103,7 @@ class Member:
     # func to check out books
     def checkOut(self):
         # make sure to add the Media item to the Media.checked_out list for inventory purposes
-        #I'm not sure if this line is correct but I think this is how to add the media to the checked_out list
+        # I'm not sure if this line is correct but I think this is how to add the media to the checked_out list
         Media.checked_out.append(self)
         pass
 
@@ -117,17 +117,23 @@ class Member:
 def displayStats():
     print("******************************************")
     print("Record of library:")
+    # for media in sorted(Media.library):
+    #     print(media)
     print("Total number of books:", Book.book_count)  # add on the number of books
     print("Number of books checked out:")
     print("Total number of videos:", Video.video_count)
     print("Total number of members:", len(Member.members))
     print("******************************************")
     print("The following items are checked out of the library:")
-        #insert checked out items + stats
+    # insert checked out items + stats
 
 
 # this is the test case, type test conditions in this function
 # and leave this function at the bottom of the file. change as
 # desired for personal unit tests
 if __name__ == "__main__":
+    book1 = Book("Bookie Boi", "Frank Hernandez", "Random House", 6)
+    book2 = Book("Bookie Boi", "Frank Hernandez", "Random House", 6)
+    vid1 = Video("Bookie Boi", "Frank Hernandez", "Random House", 80)
     displayStats()
+    print(Media.library)
