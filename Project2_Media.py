d8c8acd939ad2ae942a49747d1e2c9ea6bb79a3e
rojasjosiah <rojasjosiah@gmail.com>
2019-05-07 10:40:16 -0700

    Final comments

--- Project2_Media.py
+++ Project2_Media.py
@@ -2,41 +2,38 @@
 
 # Project Members: Josiah Rojas, April Picato, Philip Ramirez, Ali Mourad, Miguel Zavala
 
-'''
-Project 2 - Group 2
-CECS 174 TuTh 12:30
-
-Josiah Rojas: (Project Manager)
-    - Created base class Media and sub classes Book and Video
-    - Contributed to comments of Media, Book, and Video classes
-    - Added framework class Member and its basic methods
-    - Edited displayStats function
-    - Contributing to editing program for redundancy
-    - Contributed to bugfixing
-April Picato:
-    - Contributed to adding/removing media components
-    - Contributed to methods of the Book/ Video classes
-    - Contributed to methods of the Member class 
-    - Contributed to displayStats with basic print statements for desired output
-Philip Ramirez:
-    - Contributed to __repr__method and Member class formatting
-    - Contributed to __init__ and created count for classes
-    - Contributed to checkIn/checkOut functions to match expected output
-    - Contributed to displayStates and overall output formatting
-    - Fixed member items handling
-Ali Mourad:
-    - Contributed to adding/removing media components
-    - Contributed to comments of all classes
-    - Contributed to methods of Media class
-    - Contributed to methods of book/video classes
-    - Created __repr__ method of all classes
-Miguel Zavala:
-    - Contributed to Program debugging
-    - Contributed to methods of the Member class
-    - Created method to display stats and contributed to its print formatting
-    - Edited displayStats function
-
-'''
+# Project 2 - Group 2
+# CECS 174 TuTh 12:30
+#
+# Josiah Rojas: (Project Manager)
+#     - Created base class Media and sub classes Book and Video
+#     - Contributed to comments of Media, Book, and Video classes
+#     - Added framework class Member and its basic methods
+#     - Edited displayStats function
+#     - Contributing to editing program for redundancy
+#     - Contributed to bug fixing
+# April Picato:
+#     - Contributed to adding/removing media components
+#     - Contributed to methods of the Book/ Video classes
+#     - Contributed to methods of the Member class
+#     - Contributed to displayStats with basic print statements for desired output
+# Philip Ramirez:
+#     - Contributed to __repr__method and Member class formatting
+#     - Contributed to __init__ and created count for classes
+#     - Contributed to checkIn/checkOut functions to match expected output
+#     - Contributed to displayStates and overall output formatting
+#     - Fixed member items handling
+# Ali Mourad:
+#     - Contributed to adding/removing media components
+#     - Contributed to comments of all classes
+#     - Contributed to methods of Media class
+#     - Contributed to methods of book/video classes
+#     - Created __repr__ method of all classes
+# Miguel Zavala:
+#     - Contributed to Program debugging
+#     - Contributed to methods of the Member class
+#     - Created method to display stats and contributed to its print formatting
+#     - Edited displayStats function
 
 
 # overarching media class, parent for classes Book and Video
@@ -54,29 +51,28 @@ class Media:
         # Method is used to add books/videos to be checked out
         # PS objects should be pass through here
 
-    # Multiple of the same media may be added to the library, such as multiple of the same book
     def addMedia(self):
         Media.library.append(self)
 
     # Method is used to remove books/videos from library
-    # Removing a media removes 1 copy of that media
-    # if no other copy exists then the media is completely removed from library
-
+    # if no media exists then prints that
     def removeMedia(self):
         if self in Media.library:
             Media.library.remove(self)
+            self.count -= 1
         else:
             print('Media does not exist')
 
 
 # class Book child of class Media, creates Book type for library
 class Book(Media):
-    book_count = 0
+    # keeps track of how many total books exist
+    count = 0
 
     def __init__(self, title, author, publisher, num_pages):
         super().__init__(title, author, publisher)
         self.num_pages = num_pages
-        Book.book_count += 1
+        Book.count += 1
         # changed to descr so wouldn't have to know type of media to use
         self.descr = "Author: %s, Publisher: %s, Number of Pages: %d" % (author, publisher, num_pages)
 
@@ -87,12 +83,12 @@ class Book(Media):
 
 # class Video child of class Media, creates Video type for library
 class Video(Media):
-    video_count = 0
+    count = 0
 
     def __init__(self, title, author, publisher, run_time):
         super().__init__(title, author, publisher)
         self.run_time = "{} mins video".format(run_time)
-        Video.video_count += 1
+        Video.count += 1
         self.descr = "Author: %s, Publisher: %s, Run time: %d minutes" % (author, publisher, run_time)
 
     # Used to represent class object as their attributes instead of memory location
@@ -134,12 +130,14 @@ class Member:
         if a_media not in Media.checked_out:
             print('Media not yet checked out')
         else:
+            # removes from checked_out dict and items_out list
             del Media.checked_out[a_media]
             self.items_out.remove(a_media)
             print(self, "checked in:", a_media)
 
     def printCheckedOutItems(self):
         print("Items checked out by {}:".format(self))
+        # prints out each item checked out
         if len(self.items_out) > 0:
             for i in range(len(self.items_out)):
                 print('\t', self.items_out[i])
@@ -150,9 +148,9 @@ class Member:
 def displayStats():
     print("******************************************")
     print("Record of library:")
-    print("Total number of books:", Book.book_count)
+    print("Total number of books:", Book.count)
     print("Number of books checked out:", len(Media.checked_out))
-    print("Total number of videos:", Video.video_count)
+    print("Total number of videos:", Video.count)
     print("Total number of members:", len(Member.members))
     print("******************************************")
     print("The following items are checked out of the library:", *Media.checked_out, sep='\n')
@@ -186,7 +184,4 @@ if __name__ == "__main__":
     Jim.checkOut(book1)
     
     displayStats()
-    
-    # print(Media.checked_out)
-    # print(Media.library)
-    # print(Member.members)
+
